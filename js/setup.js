d25251383c4bd16045871edffca0e1be9e8a6c42
Jeremy Karlsson <karlsson@jeremy.se>
2015-09-28 09:16:53 +0200

    Fixed tables and playbuttons

--- js/setup.js
+++ js/setup.js
@@ -1,22 +1,2 @@
-/*DZ.init({
-  appId: '164985',
-  channelUrl: 'http://enjikaka.github.com/deezer-assignment/channel.html'
-});
-
-
-DZ.init({
-  appId: '164935',
-  channelUrl: 'http://localhost:3000/channel.html'
-});*/
-
-// Login stuff. Was not needed after all...
-/*DZ.login(function(response) {
-  App.tmp.token = response.authResponse.accessToken;
-  
-  DZ.api('/user/me', function(response) {
-    
-  });
-});*/
-
 App.Instance.suggestionsView = new App.View.Suggestions({model: new App.Model.Suggestion()});
 Backbone.history.start();
