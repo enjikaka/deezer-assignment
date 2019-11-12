dd692b8c8270cd61ba762afc84d808d2c2243a75
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2019-11-12 15:43:51 +0100

    Revert accidental changes in router.js

--- js/router.js
+++ js/router.js
@@ -9,14 +9,17 @@ App.Instance.appRouter = new App.Router.Default();
 
 App.Instance.appRouter.on('route:getAlbums', function(artistId) {
   App.Instance.albumListView = new App.View.AlbumList({
-    collection: new App.Collection.Album([], { artistId })
+    collection: new App.Collection.Album([], {
+      artistId: artistId
+    })
   });
 });
 
 App.Instance.appRouter.on('route:getAlbum', function(albumId) {
   App.Instance.trackListView = new App.View.TrackList({
     collection: new App.Collection.Track([], {
-      albumId: albumId}),
+      albumId: albumId
+    }),
     albumId: albumId
   });
 });
