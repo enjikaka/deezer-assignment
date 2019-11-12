cdba9603477532884ac3f021ab6f31000095e972
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2019-11-12 15:39:28 +0100

    Fix icons on https

--- js/router.js
+++ js/router.js
@@ -9,17 +9,14 @@ App.Instance.appRouter = new App.Router.Default();
 
 App.Instance.appRouter.on('route:getAlbums', function(artistId) {
   App.Instance.albumListView = new App.View.AlbumList({
-    collection: new App.Collection.Album([], {
-      artistId: artistId
-    })
+    collection: new App.Collection.Album([], { artistId })
   });
 });
 
 App.Instance.appRouter.on('route:getAlbum', function(albumId) {
   App.Instance.trackListView = new App.View.TrackList({
     collection: new App.Collection.Track([], {
-      albumId: albumId
-    }),
+      albumId: albumId}),
     albumId: albumId
   });
 });
